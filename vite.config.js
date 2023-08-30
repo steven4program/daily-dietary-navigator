a9d6e58d8e9cb8415d45c45948f063e221595ddd
steven.chang <steven.chang@ringus-solution.com>
2023-08-30 21:53:06 +0800

    fix: vite config base route

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite';
-import { ViteEjsPlugin } from 'vite-plugin-ejs';
-import { fileURLToPath } from 'node:url';
-import path from 'node:path';
-import { glob } from 'glob';
+import { defineConfig } from 'vite'
+import { ViteEjsPlugin } from 'vite-plugin-ejs'
+import { fileURLToPath } from 'node:url'
+import path from 'node:path'
+import { glob } from 'glob'
 
-import liveReload from 'vite-plugin-live-reload';
+import liveReload from 'vite-plugin-live-reload'
 
 function moveOutputPlugin() {
   return {
@@ -14,26 +14,26 @@ function moveOutputPlugin() {
     async generateBundle(options, bundle) {
       for (const fileName in bundle) {
         if (fileName.startsWith('pages/')) {
-          const newFileName = fileName.slice('pages/'.length);
-          bundle[fileName].fileName = newFileName;
+          const newFileName = fileName.slice('pages/'.length)
+          bundle[fileName].fileName = newFileName
         }
       }
-    },
-  };
+    }
+  }
 }
 
 export default defineConfig({
   // base 的寫法：
   // base: '/Repository 的名稱/'
-  base: '/web-layout-training-vite/',
+  base: '/daily-dietary-navigator/',
   plugins: [
     liveReload(['./layout/**/*.ejs', './pages/**/*.ejs', './pages/**/*.html']),
     ViteEjsPlugin(),
-    moveOutputPlugin(),
+    moveOutputPlugin()
   ],
   server: {
     // 啟動 server 時預設開啟的頁面
-    open: 'pages/index.html',
+    open: 'pages/index.html'
   },
   build: {
     rollupOptions: {
@@ -41,11 +41,14 @@ export default defineConfig({
         glob
           .sync('pages/**/*.html')
           .map((file) => [
-            path.relative('pages', file.slice(0, file.length - path.extname(file).length)),
-            fileURLToPath(new URL(file, import.meta.url)),
+            path.relative(
+              'pages',
+              file.slice(0, file.length - path.extname(file).length)
+            ),
+            fileURLToPath(new URL(file, import.meta.url))
           ])
-      ),
+      )
     },
-    outDir: 'dist',
-  },
-});
+    outDir: 'dist'
+  }
+})
