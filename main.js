df022bc0fde1cb38913276e8506c12fcc80efd36
林炘秀 <jenny031054@gmail.com>
2023-09-13 20:42:22 +0800

    commit message

--- main.js
+++ main.js
@@ -1,2 +1,11 @@
 import './assets/scss/all.scss'
 import 'bootstrap/dist/js/bootstrap.min.js'
+
+$(document).ready(function(){
+    $('#myselection').on('change', function(){
+    	var demovalue = $(this).val(); 
+        $("img.option-list").hide();
+        $("img.option-first").hide();
+        $("#show"+demovalue).show();
+    });
+});
